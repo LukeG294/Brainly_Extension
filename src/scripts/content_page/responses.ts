b84a72213ca438d91db7fa88ef08cfd6661a51bd
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-24 09:38:27 -0700

    Update responses.ts

--- src/scripts/content_page/responses.ts
+++ src/scripts/content_page/responses.ts
@@ -11,8 +11,10 @@ import {
     confirm_selected_answers,
     get_reported_content
 } from "./content_page_buttons"
+import { getPermissions } from "../common/permission_system"
 
 export function addResponseButtons(){
+    add_icons();
     let content = document.querySelectorAll("#content-old > div:nth-child(2) > div:nth-child(25) > table > tbody > tr")
 
     for (let i = 0; i < content.length; i++) {
@@ -28,27 +30,49 @@ export function addResponseButtons(){
     let buttonArea = document.querySelector("#content-old > div:nth-child(3) > p")
         //if you want to add permissions for each button later, do it here (below)
     let url = String(window.location.href)
-    buttonArea.insertAdjacentHTML('afterend', deletion_menu())
-    buttonArea.insertAdjacentHTML('afterend', confirm_selected_answers())
-    buttonArea.insertAdjacentHTML('afterend', approve_selected())
-    buttonArea.insertAdjacentHTML('afterend', delete_selected_answers())
-    buttonArea.insertAdjacentHTML('afterend', unverify_selected())
-    buttonArea.insertAdjacentHTML('afterend', copy_links())
-    buttonArea.insertAdjacentHTML('afterend', toggle_selected())
-    buttonArea.insertAdjacentHTML('afterend', select_all())
-    buttonArea.insertAdjacentHTML('afterend', get_reported_content())
-    document.getElementById("selectAll").addEventListener("click", function(){selectAll()})
-    document.getElementById("toggleSelected").addEventListener("click", function(){toggleSelection()})
-    document.getElementById("copyLinks").addEventListener("click", function(){copyLinks()})
-    document.querySelector("#deleteSelected").addEventListener("click", function(){showDelrsn("answers")})
-    document.querySelector("#delete").addEventListener("click",function(){confirmDeletionAnswers()})
-    document.querySelector("#unverify").addEventListener("click",function(){unverifyAnswers()})
-    document.querySelector("#approveSelected").addEventListener("click",function(){approveAnswers()})
-    document.querySelector("#confirmSelectedAnswers").addEventListener("click",function(){confirmAnswers()})
+
+
+    async function checkPermissionSet(){
+        let permissionSet = await getPermissions()
+        if (permissionSet >= 5){
+            buttonArea.insertAdjacentHTML('afterend', deletion_menu())
+            buttonArea.insertAdjacentHTML('afterend', confirm_selected_answers())
+            buttonArea.insertAdjacentHTML('afterend', approve_selected())
+            buttonArea.insertAdjacentHTML('afterend', delete_selected_answers())
+            document.querySelector("#deleteSelected").addEventListener("click", function(){showDelrsn("answers")})
+            document.querySelector("#delete").addEventListener("click",function(){confirmDeletionAnswers()})
+            buttonArea.insertAdjacentHTML('afterend', get_reported_content())
+            document.querySelector("#fetchReported").addEventListener("click",async function(){
+                await find_reported_content(id,"responses");
+               
+            })
+            document.querySelector("#approveSelected").addEventListener("click",function(){approveAnswers()})
+            document.querySelector("#confirmSelectedAnswers").addEventListener("click",function(){confirmAnswers()})
+        
+        } 
+        if (permissionSet >= 6){
+            buttonArea.insertAdjacentHTML('afterend', unverify_selected())
+            document.querySelector("#unverify").addEventListener("click",function(){unverifyAnswers()})
+        }
+        if (permissionSet >= 4){
+            buttonArea.insertAdjacentHTML('afterend', copy_links())
+            buttonArea.insertAdjacentHTML('afterend', toggle_selected())
+            buttonArea.insertAdjacentHTML('afterend', select_all())
+            document.getElementById("selectAll").addEventListener("click", function(){selectAll()})
+            document.getElementById("toggleSelected").addEventListener("click", function(){toggleSelection()})
+            document.getElementById("copyLinks").addEventListener("click", function(){copyLinks()})
+        }
+    }
+    checkPermissionSet()
+    
+    
+    
+    
+   
+    
+    
+    
     let id = window.location.href.replace("https://brainly.com/users/user_content/","").split("/")[0]
-    document.querySelector("#fetchReported").addEventListener("click",async function(){
-        await find_reported_content(id,"responses");
-       
-    })
+    
 }
 

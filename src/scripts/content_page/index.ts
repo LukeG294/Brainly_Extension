683348db303ec9fd0a4dd4f2771a9d3b54727974
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-24 09:38:40 -0700

    Update index.ts

--- src/scripts/content_page/index.ts
+++ src/scripts/content_page/index.ts
@@ -1,22 +1,37 @@
 import {addResponseButtons} from "./responses"
 import {addTaskButtons} from "./tasks"
 import {addticket} from "./button_functions"
-import { add_icons } from "./content_page_buttons"
+import { getPermissions } from "../common/permission_system"
 
-add_icons()
-//tasks page
-if(window.location.href.includes("task") || (!window.location.href.includes("responses") && !window.location.href.includes("comments_tr"))){
-    addTaskButtons()
-    console.log("taskpage")
+
+
+
+    //tasks page
+    if(window.location.href.includes("task") || (!window.location.href.includes("responses") && !window.location.href.includes("comments_tr"))){
+        addTaskButtons()
+        console.log("taskpage")
+    }
+
+    //responses page
+    if(window.location.href.includes("responses")){
+        addResponseButtons()
+    }
+
+    //comments page
+    if(window.location.href.includes("comments_tr")){
+
+    }
+    
+
+
+   
+
+
+async function checkPermissionSet(){
+  let permissionSet = await getPermissions()
+  if (permissionSet >= 4){
+    addticket()
+    
+  }
 }
-
-//responses page
-if(window.location.href.includes("responses")){
-    addResponseButtons()
-}
-
-//comments page
-if(window.location.href.includes("comments_tr")){
-
-}
-addticket()
+checkPermissionSet()

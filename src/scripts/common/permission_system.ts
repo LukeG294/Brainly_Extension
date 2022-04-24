a0dab965570e59dd8d3dc0521e35e27804a90f41
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-24 16:10:09 -0700

    Update permission_system.ts

--- src/scripts/common/permission_system.ts
+++ src/scripts/common/permission_system.ts
@@ -1,31 +1,53 @@
-export async function getPermissions(){
+export async function getPermissions(username,password){
          
-        /*
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        7 - Admins
-        6 - Supers
-        5 - Seniors
-        4 - Fulls
-        3 - Juniors
-        2 - Part-Time Mods
-        1 - Answerers
-        0 - None found
+    var raw = JSON.stringify({
+    "username": username,
+    "password": password
+    });
 
-        */
-        
-        let permissions = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 18:0, 82:0, 78:0, 70:0, 65:0, 40:0, 81:1, 73:1, 12:1, 66:1, 69:2, 10:3, 9:4, 56:5, 55:6, 13:7, 62:7, 8:7}
-        let txt = await fetch("https://brainly.com/api/28/api_users/me").then(data => data.json());
-        let ranks = txt["data"]["user"]["ranks_ids"]
-        let userTypes = []
-        for (let index = 0; index < ranks.length; index++) {
-            let rankID = ranks[index]
-            userTypes.push(permissions[rankID])
-        
-        }
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+   
+    };
 
-            return Math.max(...userTypes)
+    let txt = await fetch("https://th-extension.lukeg294.repl.co/login", requestOptions)
+    .then(data => data.text())
+    
+    
+    
+    if (txt){
+        let secret = JSON.parse(txt).secret.secret;
+    
+    let responseJSON = JSON.parse(txt)
+      let documentID = responseJSON.secret.instance["@ref"].id
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("fauna-secret", secret);
+      var request = {
+        method: 'GET',
+        headers: myHeaders,
+       
+       
+      };
       
-         
+    let resp = await fetch("https://th-extension.lukeg294.repl.co/users/"+documentID,request).then(data => data.text());
+    let permissionsResponseJSON = JSON.parse(resp)
+    return permissionsResponseJSON["data"]["permissions"]
+ 
+      
+    
+    }
+  
+    
+    
+    
+      
+     
      
 
 }

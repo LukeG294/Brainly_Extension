70d10e55927b115ba7388e89b08cdff5af64c916
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-24 21:25:17 -0700

    Update macc-d_exp.ts

--- src/scripts/common/macc-d_exp.ts
+++ src/scripts/common/macc-d_exp.ts
@@ -59,3 +59,17 @@ export function mmsg_s(){
     </div>
     `)
 }
+
+export function mcompu(){
+    return(/*html*/`
+    <div class="modal_back">
+
+        <div class="modal_mcomp_u">
+       
+            <h1 class="sg-text-bit--gray-secondary sg-headline sg-headline--xlarge sg-headline--extra-bold" style = "color:#323c45;">Manage Brainly Companion Users</h1>
+            <div class = "modal_close"><div class="sg-toplayer__close" role="button" tabindex="0"><div class="sg-icon sg-icon--icon-gray-50 sg-icon--x24"><svg class="sg-icon__svg" role="img" aria-labelledby="title-close-zvtc08" focusable="false"><title id="title-close-zvtc08">close</title><use xlink:href="#icon-close" aria-hidden="true"></use></svg></div></div></div>
+           
+        </div>
+    </div>
+    `)
+}

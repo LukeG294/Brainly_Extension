23726df2345977a4f1b894c8992752a85403b3a2
Saransh-255 <saranshsharma255@gmail.com>
2022-04-26 02:33:36 +0530

    broken observer

--- src/scripts/homepage/homepage.ts
+++ src/scripts/homepage/homepage.ts
@@ -13,7 +13,7 @@ async function homeperms(){
   }
 }
 //checkUser("new", homeperms)
-AnsObserver()
+//AnsObserver()
 
 //if user does not have username and password in local storage
 if(!localStorage.getItem("userAuth")){

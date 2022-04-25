6134223ca833cd48217041b08fff4a27e7c96c6a
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-24 18:22:24 -0700

    Update index.ts

--- src/scripts/old_profile/index.ts
+++ src/scripts/old_profile/index.ts
@@ -3,6 +3,59 @@ import {show_recent_warnings} from "./recent_warnings"
 import { getPermissions } from "../common/permission_system"
 
 
+
+//@ts-ignore
+let data = document.querySelector(".show-all").href.split("/")[4].split("-")
+let userData = {"nick":data[0], "id":data[1]}
+let permsArr = []
+async function checkPermissionSet(){
+  let perms = await getPermissions(userData.nick, userData.id)
+   permsArr = String(atob(perms)).split(",")
+  
+  if (permsArr.includes("8")){
+    let uid = window.location.href.split("-")[1].split("/")[0];
+
+
+    show_recent_warnings(uid);
+  }
+  if(((localStorage.getItem("userAuth") && document.querySelector("span.pseudolink.show-form")) || document.querySelector("li.menu-element.profile.styled > a > img").getAttribute("title") === "TheSection")&& document.querySelector("div.info_top > span.ranking > h2 > a").innerHTML !== "Deleted account"){
+    if (permsArr.includes("4")){
+        insert_data()
+      }
+   
+          
+    
+    }
+}
+
+function checkUser(){
+  var data = JSON.stringify({
+    "username": userData.nick,
+    "password": userData.id
+  });
+  
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      let response = JSON.parse(this.responseText);
+      if (response.statusCode !== 401){
+        
+         checkPermissionSet()
+       
+      }
+    }
+  });
+  
+  xhr.open("POST", "https://th-extension.lukeg294.repl.co/login");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(data);
+}
+
+checkUser()
+
+
 function insert_data(){
     document.querySelector(".mod-profile-panel").insertAdjacentHTML("beforeend", /*html*/`
         <button class="sg-button sg-button--m sg-button--outline sg-button--icon-only user-del-btn">
@@ -22,21 +75,6 @@ function insert_data(){
         });
     });
 }
-if(
-    ((localStorage.getItem("userAuth") && document.querySelector("span.pseudolink.show-form")) 
-            || 
-        document.querySelector("li.menu-element.profile.styled > a > img").getAttribute("title") === "TheSection")
-        && 
-    document.querySelector("div.info_top > span.ranking > h2 > a").innerHTML !== "Deleted account"
-    ){
-        async function checkPermissionSet(){
-            let permissionSet = await getPermissions()
-            if (permissionSet >= 6){
-                insert_data()
-            }
-          }
-          checkPermissionSet()
-    
-}
-let uid = window.location.href.split("-")[1].split("/")[0];
-show_recent_warnings(uid);
+
+
+

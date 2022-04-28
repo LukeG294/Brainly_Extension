46cb1ad6924b09afb3481f4b1cd4e37254e4fd3c
Saransh-255 <saranshsharma255@gmail.com>
2022-04-28 11:00:23 +0530

    user management

--- src/scripts/setPerms.ts
+++ src/scripts/setPerms.ts
@@ -2,7 +2,7 @@ import {checkPermissionSet} from "./common/permission_system"
 setTimeout(async () => {
     let permsArr = await checkPermissionSet();
     localStorage.setItem("userPerms", permsArr.toString())
-    if(permsArr[0] !== "null"){
+    if(localStorage.userPerms !== ""){
         localStorage.setItem("canUse", "true")
     }
     else{localStorage.setItem("canUse", "false")}

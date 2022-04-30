b7b4b29576a3472f93f999e9ee952bf0d2c83b11
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-30 10:14:17 -0700

    Update homepage.ts

--- src/scripts/webpages/homepage/homepage.ts
+++ src/scripts/webpages/homepage/homepage.ts
@@ -2,7 +2,7 @@ import {ryver_notification} from "../../common/Ryver/ryver_modal"
 import {login_run} from "../../common/Ryver/ryver_login"
 import { ModObserver, AnsObserver, HomeMod } from "./homepage_exports";
 //import {subscribe, setAuth} from "../common/livemod"
-import { mass_msg, mass_accdel, usr_mgmt } from "./homepage_admin";
+import { mass_msg, mass_accdel, usr_mgmt, verification_queue } from "./homepage_admin";
 import { showMessage } from "../../common/common_functions";
 
 //@ts-ignore

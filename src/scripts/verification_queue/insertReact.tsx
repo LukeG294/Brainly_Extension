7ad471bb934b2d9d4f614106f645e6f19ef23eb4
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-05-02 16:43:09 -0700

    Update insertReact.tsx

--- src/scripts/verification_queue/insertReact.tsx
+++ src/scripts/verification_queue/insertReact.tsx
@@ -3,6 +3,11 @@ import App from "./App";
 
 export async function InjectReactApp() {
   const root = ReactDOM.createRoot(document.getElementById("app"));
-
-  root.render(<App />);
+  let perms = localStorage.getItem("userPerms");
+  if (perms.includes("14")){
+    root.render(<App />);
+  } else {
+    window.location.href = '/'
+  }
+  
 }

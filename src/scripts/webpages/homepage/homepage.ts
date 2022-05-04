8b428b3af2e9c2c0cb4e5d69a59692a095ef812a
Luke Graham <lukepascalgraham1@gmail.com>
2022-05-04 16:30:54 -0700

    add back v queue btn

--- src/scripts/webpages/homepage/homepage.ts
+++ src/scripts/webpages/homepage/homepage.ts
@@ -2,7 +2,7 @@ import {ryver_notification} from "../../common/Ryver/ryver_modal"
 import {login_run} from "../../common/Ryver/ryver_login"
 import { ModObserver, AnsObserver } from "./homepage_exports";
 //import {subscribe, setAuth} from "../common/livemod"
-import { mass_msg, mass_accdel, usr_mgmt } from "./homepage_admin";
+import { mass_msg, mass_accdel, usr_mgmt, verification_queue } from "./homepage_admin";
 import { showMessage } from "../../common/common_functions";
 
 //@ts-ignore
@@ -20,6 +20,9 @@ async function homeperms(){
   }
   if(perms.includes("100")){
     usr_mgmt()
+  }
+  if(perms.includes("14")){
+    verification_queue()
   }
   else{
     AnsObserver()

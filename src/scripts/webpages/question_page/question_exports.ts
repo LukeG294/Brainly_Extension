e249306eb00600953923c25afaacea6df941f963
Luke Graham <lukepascalgraham1@gmail.com>
2022-05-05 13:11:36 -0700

    already requested button

--- src/scripts/webpages/question_page/question_exports.ts
+++ src/scripts/webpages/question_page/question_exports.ts
@@ -26,70 +26,89 @@ export function confirmButton(){
     }
 }
 
-export function requestApproval(){
+export async function requestApproval(){
   let answers = document.querySelectorAll("div[data-testid = 'moderation_box_answer'] > div")
   let responses = JSON.parse(document.querySelector("[data-testid='question_box']").getAttribute("data-z")).responses
     for (let i = 0; i < answers.length; i++) {
      if (!responses[i].approved.date){
-      answers[i].insertAdjacentHTML("beforeend",`<button class="sg-button sg-button--m sg-button--solid-mint  request-verification"> <div class="spinner-container">
-      <div class="sg-spinner sg-spinner--gray-900 sg-spinner--xsmall"></div>
-  </div><span class="sg-button__icon sg-button__icon--m">
-      <div class="sg-icon sg-icon--adaptive sg-icon--x24"><svg class="sg-icon__svg" role="img" aria-labelledby="title-heart_outlined-pld9rg" focusable="false"><text id="title-heart_outlined-pld9rg" hidden="">heart outlined</text>
-          <use xlink:href="#icon-check" aria-hidden="true"></use>
-        </svg></div>
-    </span><span class="sg-button__text">Request Verification</span></button>`)
+       let answer = responses[i].id
+       let resp = await fetch("https://TH-Extension.lukeg294.repl.co/get_answer_by_id/"+answer).then(response => response.json())
+      //if cannot find previous request then add new button
+       if (!resp.data[0]){
+       
+            answers[i].insertAdjacentHTML("beforeend",`<button class="sg-button sg-button--m sg-button--solid-mint  request-verification"> <div class="spinner-container">
+            <div class="sg-spinner sg-spinner--gray-900 sg-spinner--xsmall"></div>
+        </div><span class="sg-button__icon sg-button__icon--m">
+            <div class="sg-icon sg-icon--adaptive sg-icon--x24"><svg class="sg-icon__svg" role="img" aria-labelledby="title-heart_outlined-pld9rg" focusable="false"><text id="title-heart_outlined-pld9rg" hidden="">heart outlined</text>
+                <use xlink:href="#icon-check" aria-hidden="true"></use>
+              </svg></div>
+          </span><span class="sg-button__text">Request Verification</span></button>`)
+          let requestButtons = document.querySelectorAll(".request-verification")
+    
+          for (let index = 0; index < requestButtons.length; index++) {
+            const element = requestButtons[index];
+            
+            element.addEventListener("click", async function(){
+              document.querySelector(".request-verification .spinner-container").classList.add("show");
+              let thisResponse = responses[index]
+            
+              let databaseId = thisResponse.id
+              let answerPreview = thisResponse.content
+              let qinfo = JSON.parse(document.querySelector("article").getAttribute("data-z"))
+              //@ts-ignore
+              let requesterID = JSON.parse(document.querySelector("meta[name='user_data']").content).id
+              var myHeaders = new Headers();
+              myHeaders.append("Content-Type", "application/json");
+              let usr = new User()
+              let user = await usr.Data(thisResponse.userId)
+              console.log(user)
+              var raw = JSON.stringify({
+                "settings": thisResponse,
+                "answerDBid":databaseId,
+                "content":answerPreview,
+                "qid": qinfo.id,
+                "subject":document.querySelector("a[data-testid = 'question_box_subject']").innerHTML,
+                "user": user,
+               
+              });
+            
+              var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw
+            
+              };
+
+                let serverResponse = await fetch("https://th-extension.lukeg294.repl.co/request-verify-add", requestOptions).then(response => response.json())
+              
+                //@ts-ignore
+                console.log(serverResponse)
+                if (serverResponse){
+                  showMessage("The answer has been added to the verification queue.","success")
+                  document.querySelector(".request-verification .spinner-container").classList.remove("show");
+                } else {
+                  showMessage("There was an error adding to the verification queue.","error")
+                }
+                
+            })
+            
+          }
+          //else - add already requested button
+       } else {
+         
+            answers[i].insertAdjacentHTML("beforeend",` <div class="sg-flex sg-flex--column"><button style="margin-bottom:12px; opacity:0.6; pointer-events: none;" class="sg-button sg-button--m sg-button--solid-light sg-button--solid-light-toggle-blue"><span class="sg-button__icon sg-button__icon--m">
+            <div class="sg-icon sg-icon--adaptive sg-icon--x24"><svg class="sg-icon__svg" role="img" aria-labelledby="title-heart-215qb" focusable="false"><text id="title-heart-215qb" hidden="">heart</text>
+                <use xlink:href="#icon-send" aria-hidden="true"></use>
+              </svg></div>
+          </span><span class="sg-button__text">Verification Requested</span></button></div>`)
+
+       }
+      
+     
      }
     
     }
 
-    let requestButtons = document.querySelectorAll(".request-verification")
     
-    for (let index = 0; index < requestButtons.length; index++) {
-      const element = requestButtons[index];
-      
-      element.addEventListener("click", async function(){
-        document.querySelector(".request-verification .spinner-container").classList.add("show");
-        let thisResponse = responses[index]
-       
-        let databaseId = thisResponse.id
-        let answerPreview = thisResponse.content
-        let qinfo = JSON.parse(document.querySelector("article").getAttribute("data-z"))
-        //@ts-ignore
-        let requesterID = JSON.parse(document.querySelector("meta[name='user_data']").content).id
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        let usr = new User()
-        let user = await usr.Data(thisResponse.userId)
-        console.log(user)
-        var raw = JSON.stringify({
-          "settings": thisResponse,
-          "answerDBid":databaseId,
-          "content":answerPreview,
-          "qid": qinfo.id,
-          "subject":document.querySelector("a[data-testid = 'question_box_subject']").innerHTML,
-          "user": user,
-        });
-      
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: raw
-       
-        };
-
-          let serverResponse = await fetch("https://th-extension.lukeg294.repl.co/request-verify-add", requestOptions).then(response => response.json())
-         
-          //@ts-ignore
-          console.log(serverResponse)
-          if (serverResponse){
-            showMessage("The answer has been added to the verification queue.","success")
-            document.querySelector(".request-verification .spinner-container").classList.remove("show");
-          } else {
-            showMessage("There was an error adding to the verification queue.","error")
-          }
-          
-      })
-      
-    }
 }
 

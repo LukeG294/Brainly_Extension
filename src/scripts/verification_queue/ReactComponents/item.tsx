8c45703f1d79d508b8321c42ad421e9a923de7a3
Saransh-255 <saranshsharma255@gmail.com>
2022-05-06 03:26:22 +0530

    attachment icon

--- src/scripts/verification_queue/ReactComponents/item.tsx
+++ src/scripts/verification_queue/ReactComponents/item.tsx
@@ -19,18 +19,31 @@ interface Item{
 export default function Item({ content, thanks, rating, created, ansdata, faunadbid, answerer}: Item) {
     return(
         <div className = "item" datatype = {ansdata.settings.id}>
+            <div className="spinner-container"><Spinner /></div>
             <div className="head">
                 <svg className="sg-subject-icon">
                     <use xlinkHref={"#icon-subject-"+ansdata.subject.toLowerCase()}/>
                 </svg>
                 <div className = "sg-flex flex-direction-column">
-                    <h2 className="sg-text sg-text--large sg-text--gray sg-text--bold qid">{"#"+ansdata.qid}</h2>
+                    <h2 
+                    className="sg-text sg-text--large sg-text--gray sg-text--bold qid"
+                    onClick={async () => await insert_ticket(ansdata.qid, document.querySelector(`[ datatype = '${ansdata.settings.id}']`))}
+                    >
+                        {"#"+ansdata.qid}
+                    </h2>
+
                     <h2 className="sg-text sg-text--large sg-text--gray created">{get_time_diff(created)}</h2>
                 </div>
             </div>
             <div className="content" dangerouslySetInnerHTML={{__html: content}}></div>
             
             <div className="stats">
+                <Icon
+                className= {ansdata.settings.attachments.length > 0 ? "attachment" : "no-attachment"}
+                color="adaptive"
+                size={16}
+                type="attachment"
+                />
                 <Label className = "thanks" color="achromatic" iconType="heart" type="transparent">{thanks}</Label>
                 <Label className = "rating" color="achromatic" iconType="star" type="transparent">{rating}</Label>
             </div>
@@ -63,18 +76,6 @@ export default function Item({ content, thanks, rating, created, ansdata, faunad
                         toggle="blue"
                         type="outline"
                         className = "approve"
-                        />
-                    </div>
-                    <div className="tickbut"> 
-                        <Spinner size={"xsmall"} />
-                        <Button 
-                        onClick={async () => await insert_ticket(ansdata.qid, document.querySelector(`[ datatype = '${ansdata.settings.id}'] .tickbut`))}
-                        icon={<Icon color="adaptive" size={24} type="shield"/>}
-                        iconOnly
-                        size="m"
-                        toggle="mustard"
-                        type="outline"
-                        className = "ticket"
                         />
                     </div>
                 </div>

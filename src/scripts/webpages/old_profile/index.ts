c0f1835fc0cb2af66bb7609b028d82d3fdaf75b7
Saransh-255 <saranshsharma255@gmail.com>
2022-05-07 23:30:50 +0530

    user bio design

--- src/scripts/webpages/old_profile/index.ts
+++ src/scripts/webpages/old_profile/index.ts
@@ -2,22 +2,19 @@ import { insert_request_ban } from "./request_ban";
 import {add_del_menu, delete_user_btn} from "./del_menu_exp"
 import {show_recent_warnings} from "./recent_warnings"
 import {insert_bio} from "./user_bio"
-//import { getPermissions, checkUser, checkPermissionSet } from "../common/permission_system"
 
 //@ts-ignore
 
 async function oldProfile(){
-    let perms = localStorage.userPerms.split(",")
-    if(perms.includes("8")){
-      let uid = (<string>window.location.href).split("-")[1].split("/")[0];
-      show_recent_warnings(uid)
-    }
-    if(perms.includes("4")){ 
-      delete_user_btn()
-    }
-    insert_bio()
-   
-
+  insert_bio()
+  let perms = localStorage.userPerms.split(",")
+  if(perms.includes("8")){
+    let uid = (<string>window.location.href).split("-")[1].split("/")[0];
+    show_recent_warnings(uid)
+  }
+  if(perms.includes("4")){ 
+    delete_user_btn()
+  }
 }
 
 if(localStorage.canUse === "true"){

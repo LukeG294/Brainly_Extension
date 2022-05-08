7f4e2255848783b8cac60737d8630573bd049da8
Luke Graham <lukepascalgraham1@gmail.com>
2022-05-08 16:12:29 -0700

    overwrite

--- src/scripts/views/verification_queue/insertReact.tsx
+++ src/scripts/views/verification_queue/insertReact.tsx
@@ -1,11 +1,11 @@
-import { brainly_legacy_api_url } from "configs/config";
+
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 export async function InjectReactApp() {
   const root = ReactDOM.createRoot(document.getElementById("app"));
   let perms = localStorage.userPerms.split(",")
-  let user = await fetch(`${brainly_legacy_api_url()}/api_users/me`).then(data => data.json())
+  let user = await fetch(`https://brainly.com/api/v8/api_users/me`).then(data => data.json())
   let num = user.data.user.mod_actions_count
  
   if (perms.includes("5") && num || perms.includes("4") && num){

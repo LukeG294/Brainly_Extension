6496d0b14653f43f2de7c78ee234739f4d572f95
Luke Graham <lukepascalgraham1@gmail.com>
2022-05-07 23:27:21 -0700

    '

--- src/scripts/verification_queue/insertReact.tsx
+++ src/scripts/verification_queue/insertReact.tsx
@@ -1,10 +1,11 @@
+import { brainly_legacy_api_url } from "configs/links";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 export async function InjectReactApp() {
   const root = ReactDOM.createRoot(document.getElementById("app"));
   let perms = localStorage.userPerms.split(",")
-  let user = await fetch("https://brainly.com/api/28/api_users/me").then(data => data.json())
+  let user = await fetch(`${brainly_legacy_api_url()}/api_users/me`).then(data => data.json())
   let num = user.data.user_category
   if (perms.includes("5") && num !== 100 || perms.includes("4") && num !== 100){
     root.render(<App />);

9877b10f674e1eec537d3cd4d802c24953d23895
Luke Graham <lukepascalgraham1@gmail.com>
2022-05-08 16:13:40 -0700

    28 not v8

--- src/scripts/views/verification_queue/insertReact.tsx
+++ src/scripts/views/verification_queue/insertReact.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 export async function InjectReactApp() {
   const root = ReactDOM.createRoot(document.getElementById("app"));
   let perms = localStorage.userPerms.split(",")
-  let user = await fetch(`https://brainly.com/api/v8/api_users/me`).then(data => data.json())
+  let user = await fetch(`https://brainly.com/api/28/api_users/me`).then(data => data.json())
   let num = user.data.user.mod_actions_count
  
   if (perms.includes("5") && num || perms.includes("4") && num){

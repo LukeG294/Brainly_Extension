6beb832450b40555e81c3dc9d9e2734711cfcb10
Saransh-255 <saranshsharma255@gmail.com>
2022-05-15 00:11:03 +0530

    question page new tickets

--- src/scripts/views/question_page/QuestionExports.ts
+++ src/scripts/views/question_page/QuestionExports.ts
@@ -39,17 +39,18 @@ export async function removeAnswer(id){
   }
 }
 export async function newTickets(){
-  let oldTickets = document.querySelectorAll('[title="moderate"]')
+  let oldTickets = document.querySelectorAll('[data-testid="options_list"]')
   oldTickets.forEach(element => {
-    element.insertAdjacentHTML("beforeend",/*html*/`
-    <button class="sg-button sg-button--m sg-button--solid-light sg-button--solid-light-toggle-blue openTicketQuestionPage">
+    element.insertAdjacentHTML("afterbegin",/*html*/`
+    <div class = "sg-flex sg-flex--margin-left-xxs">
+    <button class="openTicketQuestionPage">
     <div class="spinner-container"><div class="sg-spinner sg-spinner--gray-900 sg-spinner--xsmall"></div></div>
       <span class="sg-button__icon sg-button__icon--m">
         <div class="sg-icon sg-icon--adaptive sg-icon--x24"><svg class="sg-icon__svg" role="img"  focusable="false"><text id="title-add_more-9qmrbd" hidden="">filter filled</text>
             <use xlink:href="#icon-shield" aria-hidden="true"></use>
           </svg></div>
-      </span><span class="sg-button__text">moderate</span>
     </button>
+    </div>
     `)
   });
   let id = window.location.href.split('/')[4].split("?")[0]

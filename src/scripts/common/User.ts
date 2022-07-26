a424cc181598cb2720d705d8277c673aae7ad949
Saransh-255 <saranshsharma255@gmail.com>
2022-07-26 18:41:15 +0530

    md info

--- src/scripts/common/User.ts
+++ src/scripts/common/User.ts
@@ -43,4 +43,21 @@ export default new class User{
             }`
         )
     }
+    MdButton(id:string, type:string){
+        return /*html*/`
+            <form onsubmit="return confirm('Are you sure you want to continue:\n\nDelete ${type}');" id="AddForm" method="post" action="/admin/users/delete_${type}">
+                <fieldset style="display:none;">
+                    <input type="hidden" name="_method" value="POST">
+                    <input type="hidden" name="data[_Token][key]" value="05f9abe6c1df13598c39a2a0b972817daa55827b" id="Token885673562">
+                </fieldset>
+
+                <input type="hidden" name="data[uid]" value=${id} id="uid">
+                <input type="submit" class="onlylink" value="Delete ${type}">
+
+                <fieldset style="display:none;">
+                    <input type="hidden" name="data[_Token][fields]" value="eb65d6c972f557e5f1517750a0592cb46375f53b%3An%3A1%3A%7Bv%3A0%3Bf%3A3%3A%22hvq%22%3B%7D" id="TokenFields62231453">
+                    <input type="hidden" name="data[_Token][lock]" value="_PHP603da1b527604e9c206770a9f19c7fd75f6dc8ed" id="TokenLocker62231453">
+                </fieldset>
+            </form>`
+    }
 }

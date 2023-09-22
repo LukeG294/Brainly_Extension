8d7ce4b57a6356f550711083ab8ea5032706847c
Luke Graham <lukepascalgraham1@gmail.com>
2023-09-21 23:00:04 -0700

    commit

--- src/scripts/common/Permissions/get_permissions.ts
+++ src/scripts/common/Permissions/get_permissions.ts
@@ -1,3 +1,27 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
 async function getData(){
 
 
@@ -5,8 +29,15 @@ async function getData(){
     //let id = user_data.data.id
     //let nick = user_data.data.nick
     let data = await fetch(`https://lgextension.azurewebsites.net/get_user/16118329`).then(data => data.json())
+    if (getCookie("extension_permissions") !== ""){
+      alert(getCookie("extension_permissions"))
+    } else {
+      setCookie("extension_permissions", btoa(data),"30")
+     
+    }
     // do something with response here, not outside the function
-    console.log(data.permissions)
+   
+    
 }
 
 getData()

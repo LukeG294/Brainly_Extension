fa627d14641fda6c9b8e1fe2e13965d0a94d82ca
Luke Graham <lukepascalgraham1@gmail.com>
2023-09-21 22:52:11 -0700

    get firebase data

--- src/background/background.ts
+++ src/background/background.ts
@@ -12,23 +12,10 @@ class Background {
 				case 'confirm':
 					fetch(`https://lgextension.azurewebsites.net/confirm/?id=${request.data.id}&cookie=${request.data.cookie}`, {'mode': 'no-cors'})
 					return;
-				case 'get_permissions':
-					let data = await fetch(`https://lgextension.azurewebsites.net/get_user/${request.data.id}`).then(data => data.json())
-					sendResponse(data);
-					return true;
 			}
 			
 		})
-		chrome.runtime.onMessage.addListener(
-			async function(request, sender, sendResponse) {
-			  console.log(sender.tab ?
-						  "from a content script:" + sender.tab.url :
-						  "from the extension");
-			  if (request.greeting === "hello")
-			  	//var data = await fetch(`https://lgextension.azurewebsites.net/get_user/${request.data.id}`).then(data => data.json())
-				sendResponse({farewell: "goodbyde"});
-			}
-		  );
+	
 	}
 	
 	
--- src/scripts/common/Permissions/get_permissions.ts
+++ src/scripts/common/Permissions/get_permissions.ts
@@ -1,15 +1,12 @@
 async function getData(){
 
 
-    let user_data = await fetch('https://brainly.com/api/28/api_user_profiles/get_by_id/16118329').then(data => data.json())
-    let id = user_data.data.id
-    let nick = user_data.data.nick
-
-
-    //await chrome.runtime.sendMessage({data: {"id":id,"username":nick}, message:"get_permissions"}, function (response) {console.log(response)});
-    const response = await chrome.runtime.sendMessage({greeting: "hello", "id":"16118329","username":"x"});
-  // do something with response here, not outside the function
-  console.log(response);
+    //let user_data = await fetch('https://brainly.com/api/28/api_user_profiles/get_by_id/16118329').then(data => data.json())
+    //let id = user_data.data.id
+    //let nick = user_data.data.nick
+    let data = await fetch(`https://lgextension.azurewebsites.net/get_user/16118329`).then(data => data.json())
+    // do something with response here, not outside the function
+    console.log(data.permissions)
 }
 
 getData()

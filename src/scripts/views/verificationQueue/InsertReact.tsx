b18ed9d2f00ecab94f52d0ad201238a912f7fe00
Luke Graham <lukepascalgraham1@gmail.com>
2023-09-26 07:03:16 -0700

    old perms being weird, delete

--- src/scripts/views/verificationQueue/InsertReact.tsx
+++ src/scripts/views/verificationQueue/InsertReact.tsx
@@ -4,15 +4,13 @@ import App from "./App";
 
 export async function InjectReactApp() {
   const root = ReactDOM.createRoot(document.getElementById("app"));
-  let perms = localStorage.userPerms.split(",")
+//  let perms = localStorage.userPerms.split(",")
 
  
-  if (perms.includes("4")  || perms.includes("6") || perms.includes("7")){ //admin or vt mod or vt admin
+  
     document.title = 'Brainly Companion';
     root.render(<App />);
     
-  } else {
-    window.location.href = '/'
-  }
+  
   
 }
